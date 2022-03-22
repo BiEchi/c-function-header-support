9bf9d82f1453423b3821c140ecdd44dfb8b87368
Jackgetup <Haob.19@intl.zju.edu.cn>
2022-03-21 22:55:36 -0500

    added illustration

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ Just download it and you're all set
 ## Snippets
 
 Type `head` and you'll see `header` come up. Type it and you're all set.
+
+## Format
+
+The format is shown below.
+
+![image-20220321225443554](https://jacklovespictures.oss-cn-beijing.aliyuncs.com/2022-03-22-035444.png)
--- tests/test.c
+++ tests/test.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
-/* Function Name
-*Inputs:
-*/  
-
+/* main()
+ *  Functionality: this is the main function of the file
+ *  Arguments: none
+ *  Return: 0 for success
+ *  References: https://www.baidu.com
+ ***********************************************************************************
+ *  THIS FUNCTION SHOULD NOT BE MODIFIED UNLESS YOU KNOW HOW TO DO THAT
+ */
+head
 int main() {
     printf("How are you?\n");
     return 0;
